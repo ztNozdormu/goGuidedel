b71e05bb2478f61da3a496146ca2226d4f23f059
companydev <w1999wtw3537@sina.com>
2019-05-07 13:03:37 +0800

    第一个GO程序

---
+++ phaseOne/variablesAndBasicTypes/code/01第一个GO程序.go
@@ -0,0 +1,18 @@
+//导入主函数的包
+package main
+//fmt format 包含格式化输入输出 go SDK中函数
+import "fmt"
+
+
+//main 叫主函数 是程序的主入口 一个OG程序有且只有一个主函数
+func main(){
+    // 打印输出
+	fmt.Printf("hello world ,I am the first Go program!")
+	//注释  注释不参与程序编译 可以帮助可以程序
+	//行注释 只能注释一行
+	//快速注释的快捷键 ctrl+/
+	/*
+	   块注释
+	   可以注释多行
+	*/
+}
